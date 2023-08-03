f1b0613cdc7f5ba45cfb6498d5e060f0f63a9668
tayyaba-ali <ta6402792@gmail.com>
2023-08-03 20:58:34 +0500

    restart functionality remain

--- Project # 01 (cal)/index.js
+++ Project # 01 (cal)/index.js
@@ -1,1 +1,69 @@
-console.log("hello world");
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+function sleep() {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+}
+async function welcome() {
+    let title = chalkAnimation.karaoke("Let's calculate");
+    await sleep();
+    title.stop();
+    console.log(` _____________________
+|  _________________  |
+| | JO           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+
+DEPLOYED BY TAYYABA
+
+`);
+    askQuestions();
+}
+welcome();
+async function askQuestions() {
+    inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'operator',
+            message: 'Select the operator',
+            choices: ['Addition', 'Subraction', 'Multiplication', 'Division'],
+        },
+        {
+            type: 'number',
+            name: 'num1',
+            message: 'Enter num 1',
+        },
+        {
+            type: 'number',
+            name: 'num2',
+            message: 'Enter num 2',
+        },
+    ])
+        .then((answers) => {
+        let { num1, num2, operator } = answers;
+        if (operator === "Addition") {
+            console.log(`${num1}+${num2}=${num1 + num2}`);
+        }
+        else if (operator === "Subtraction") {
+            console.log(`${num1}-${num2}=${num1 - num2}`);
+        }
+        else if (operator === "Multiplication") {
+            console.log(`${num1}*${num2}=${num1 * num2}`);
+        }
+        else {
+            console.log(`${num1}/${num2}=${num1 / num2}`);
+        }
+    });
+}
